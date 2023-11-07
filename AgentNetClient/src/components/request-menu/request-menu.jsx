17e9179345c83dbf300fede2dd0c0f09c37cfae8
Luisangel Parra <lfaria@uninorte.edu.co>
2023-11-06 19:26:32 -0500

    request menu

---
+++ AgentNetClient/src/components/request-menu/request-menu.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./requeste-menu.css";
+
+export function RequestMenu() {
+    return ( 
+        <div className="main_container">
+        <div className="main_title">
+            <h1>Mis Solicitudes</h1>
+            <i className="fi-rr-square-info"></i>
+        </div>
+
+        <div className="main_box">
+            <div className="search_box">
+                <div className="search_bar">
+                    <input type="text" name="search_bar" id="search_bar" placeholder="Busca aqui por nombre, apellido, empresa, etc."/>
+                    <i className="fi-rr-search"></i>
+                </div>
+                <div className="search_button">Filtrar</div>
+            </div>
+
+            <div className="table">
+                <table>
+                    <tr>
+                        <th><input type="checkbox" name="" id="" className="check_button"/></th>
+                        <th>ID SOLICITUD</th>
+                        <th>SOLICITANTE</th>
+                        <th>FUENTE</th>
+                        <th>NOMBRE</th>
+                        <th>ESTADO</th>
+                        <th>ANEXOS</th>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" name="" id="" className="check_button"/></td>
+                        <td>1095314</td>
+                        <td>Cliente</td>
+                        <td>Perfil agente</td>
+                        <td>Marcela Valencia</td>
+                        <td>Atentido</td>
+                        <td><div className="details_button">DETALLES</div></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        
+    </div>
+     );
+}
+
+export default RequestMenu;
