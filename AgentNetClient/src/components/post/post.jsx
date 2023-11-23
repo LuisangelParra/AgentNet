c2f8b123f4045b83ae9cf6702ff3e6f6a751b125
Luisangel Parra <lfaria@uninorte.edu.co>
2023-11-23 16:19:57 -0500

    Update .gitignore and remove unnecessary .pyc files

--- AgentNetClient/src/components/post/post.jsx
+++ AgentNetClient/src/components/post/post.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect, useRef } from "react";
 import "./post.css";
 import { Link } from "react-router-dom";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  useAdvancedMarkerRef,
+} from "@vis.gl/react-google-maps";
 
 export function Post() {
+  const [markerRef, marker] = useAdvancedMarkerRef();
+  const [position, setPosition] = useState({
+    lat: 10.9838092,
+    lng: -74.8592172,
+  });
+  const [zoom, setZoom] = useState(12);
+
+  const autoCompleteRef = useRef();
+  const inputRef = useRef();
+  const options = {
+    componentRestrictions: { country: "co" },
+    fields: ["address_components", "geometry", "icon", "name"],
+    types: ["establishment"],
+  };
+
+
+  useEffect(() => {
+    autoCompleteRef.current = new window.google.maps.places.Autocomplete(
+      inputRef.current,
+      options
+    );
+
+    // Agrega un listener al evento place_changed
+    autoCompleteRef.current.addListener("place_changed", handlePlaceSelect);
+  }, []);
+
+  const handlePlaceSelect = () => {
+    // Obtiene la información del lugar seleccionado
+    const selectedPlace = autoCompleteRef.current.getPlace();
+
+    // Accede a las coordenadas (latitud y longitud)
+    const { geometry } = selectedPlace;
+    const { location } = geometry;
+    const lat = location.lat();
+    const lng = location.lng();
+    setPosition({ lat, lng });
+    setZoom(15);
+  };
+
   return (
     <div className="post-container">
       <div className="m-back">
@@ -22,35 +69,35 @@ export function Post() {
           <div className="images">
             <div className="image image1">
               <input type="file" id="file" />
-              <label for="file">
+              <label htmlFor="file">
                 <i className="fi-rr-plus"></i>
               </label>
             </div>
 
             <div className="image image2">
               <input type="file" id="file" />
-              <label for="file">
+              <label htmlFor="file">
                 <i className="fi-rr-plus"></i>
               </label>
             </div>
 
             <div className="image image3">
               <input type="file" id="file" />
-              <label for="file">
+              <label htmlFor="file">
                 <i className="fi-rr-plus"></i>
               </label>
             </div>
 
             <div className="image image4">
               <input type="file" id="file" />
-              <label for="file">
+              <label htmlFor="file">
                 <i className="fi-rr-plus"></i>
               </label>
             </div>
 
             <div className="image image5">
               <input type="file" id="file" />
-              <label for="file">
+              <label htmlFor="file">
                 <i className="fi-rr-plus"></i>
               </label>
             </div>
@@ -65,9 +112,11 @@ export function Post() {
                 name="search_bar"
                 id="search_bar"
                 placeholder="Buscar la dirección del inmueble"
+                ref={inputRef}
               />
               <i className="fi-rr-search"></i>
             </div>
+            <div className="places-container"></div>
 
             <h2>Tags</h2>
             <div className="search_bar_tags">
@@ -80,71 +129,129 @@ export function Post() {
               <i className="fi-rr-search"></i>
             </div>
             <div className="tag-list">
-              <div className="tag">Bogotá <i className="fi-rr-cross-small"></i></div>
-              <div className="tag">Departamento <i className="fi-rr-cross-small"></i></div>
-              <div className="tag">Nuevo <i className="fi-rr-cross-small"></i></div>
+              <div className="tag">
+                Bogotá <i className="fi-rr-cross-small"></i>
+              </div>
+              <div className="tag">
+                Departamento <i className="fi-rr-cross-small"></i>
+              </div>
+              <div className="tag">
+                Nuevo <i className="fi-rr-cross-small"></i>
+              </div>
             </div>
           </div>
 
-          <div className="map"></div>
+          <div className="map">
+            <APIProvider
+              apiKey={import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY}
+            >
+              <Map
+                zoom={zoom}
+                center={position}
+                id="map1"
+                mapId={import.meta.env.VITE_REACT_APP_GOOGLE_MAP_ID}
+              >
+                <AdvancedMarker ref={markerRef} position={position} />{" "}
+              </Map>
+            </APIProvider>
+          </div>
         </div>
 
         <div className="form-details">
-            <h2>Datos Generales</h2>
-            <div className="detail-input">
-                <label for="title">Título</label>
-                <input type="text" name="title" id="title" placeholder="Escribe un título para tu inmueble"/>
-            </div>
-            <div className="detail-input">
-                <label for="property-type">Tipo de inmueble</label>
-                <select name="property-type" id="">
-                    <option value="apartamento">Apartamento</option>
-                    <option value="casa">Casa</option>
-                    <option value="oficina">Oficina</option>
-                    <option value="local">Local</option>
-                    <option value="lote">Lote</option>
-                </select>
-            </div>
+          <h2>Datos Generales</h2>
+          <div className="detail-input">
+            <label htmlFor="title">Título</label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              placeholder="Escribe un título para tu inmueble"
+            />
+          </div>
+          <div className="detail-input">
+            <label htmlFor="property-type">Tipo de inmueble</label>
+            <select name="property-type" id="">
+              <option value="apartamento">Apartamento</option>
+              <option value="casa">Casa</option>
+              <option value="oficina">Oficina</option>
+              <option value="local">Local</option>
+              <option value="lote">Lote</option>
+            </select>
+          </div>
 
-            <div className="detail-input">
-                <label for="sale-type">Garaje</label>
-                <select name="sale-type" id="sale-type">
-                    <option value="sale">Vender</option>
-                    <option value="rent">Arrendar</option>
-                </select>
-            </div>
+          <div className="detail-input">
+            <label htmlFor="sale-type">Garaje</label>
+            <select name="sale-type" id="sale-type">
+              <option value="sale">Vender</option>
+              <option value="rent">Arrendar</option>
+            </select>
+          </div>
 
-            <div className="detail-input">
-                <label for="total-area">Area</label>
-                <input type="number" name="total-area" id="total-area" placeholder="Escribe el área total"/>
-            </div>
-            <div className="detail-input">
-                <label for="build-area">Area Construida</label>
-                <input type="number" name="build-area" id="build-area" placeholder="Escribe el área total construida"/>
-            </div>
-            <div className="detail-input">
-                <label for="rooms">Habitaciones</label>
-                <input type="number" name="rooms" id="rooms" placeholder="Escribe el número de habitaciones"/>
-            </div>
-            <div className="detail-input">
-                <label for="bathrooms">Baños</label>
-                <input type="number" name="bathrooms" id="bathrooms" placeholder="Escribe el número de baños"/>
-            </div>
-            <div className="detail-input">
-                <label for="garage">Garaje</label>
-                <input type="number" name="garage" id="garage" placeholder="Escribe el número de garajes"/>
-            </div>
+          <div className="detail-input">
+            <label htmlFor="total-area">Area</label>
+            <input
+              type="number"
+              name="total-area"
+              id="total-area"
+              placeholder="Escribe el área total"
+            />
+          </div>
+          <div className="detail-input">
+            <label htmlFor="build-area">Area Construida</label>
+            <input
+              type="number"
+              name="build-area"
+              id="build-area"
+              placeholder="Escribe el área total construida"
+            />
+          </div>
+          <div className="detail-input">
+            <label htmlFor="rooms">Habitaciones</label>
+            <input
+              type="number"
+              name="rooms"
+              id="rooms"
+              placeholder="Escribe el número de habitaciones"
+            />
+          </div>
+          <div className="detail-input">
+            <label htmlFor="bathrooms">Baños</label>
+            <input
+              type="number"
+              name="bathrooms"
+              id="bathrooms"
+              placeholder="Escribe el número de baños"
+            />
+          </div>
+          <div className="detail-input">
+            <label htmlFor="garage">Garaje</label>
+            <input
+              type="number"
+              name="garage"
+              id="garage"
+              placeholder="Escribe el número de garajes"
+            />
+          </div>
 
-            <div className="detail-input">
-                <label for="description">Descripción</label>
-                <input type="text" name="description" id="description" placeholder="Breve descripción"/>
-            </div>  
+          <div className="detail-input">
+            <label htmlFor="description">Descripción</label>
+            <input
+              type="text"
+              name="description"
+              id="description"
+              placeholder="Breve descripción"
+            />
+          </div>
 
-            <div className="detail-input">
-                <label for="price">Precio</label>
-                <input type="number" name="price" id="price" placeholder="Escribe el precio"/>
-            </div>    
-
+          <div className="detail-input">
+            <label htmlFor="price">Precio</label>
+            <input
+              type="number"
+              name="price"
+              id="price"
+              placeholder="Escribe el precio"
+            />
+          </div>
         </div>
       </form>
     </div>
